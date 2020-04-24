03cfb18f274a1093cbae5c82a19c813642a43b7c
hyeleelee <hgoguma124@gmail.com>
2020-04-24 18:07:35 +0900

    프로젝트 만듦

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    
+    <Header></Header>
+    <b-container >
+      
+      <List></List>
+    <!-- <Table></Table> -->
+    <!-- <Pagination></Pagination> -->
+    </b-container>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+//import Table from './components/Table.vue'
+import List from './components/List.vue'
+//import Pagination from './components/Pagination.vue'
+
+
+
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    // Table,
+    List,
+    //Pagination
+  }
+}
+</script>
---
+++ src/components/Pagination.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="overflow-auto">
+        <div class="mt-3">
+            <h6>Default</h6>
+            <b-pagination v-model="currentPage" :total-rows="rows"></b-pagination>
+        </div>
+    </div>
+</template>
